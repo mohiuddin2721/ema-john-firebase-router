fc7b30f74720a4c4d1804ded2e19c99da556e960
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-16 15:46:16 +0600

    dynamically added uder name and photo in header

--- src/Page/Shared/Header/Header.js
+++ src/Page/Shared/Header/Header.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
 import logo from '../../../images/Logo.svg'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        signOut(auth);
+        navigate('/home')
+    }
+
     return (
         <div>
             <Navbar sticky="top" collapseOnSelect expand="lg" bg="primary" variant="dark">
@@ -18,11 +30,30 @@ const Header = () => {
                             <Nav.Link as={Link} to="/home" href="#home">Home</Nav.Link>
                             <Nav.Link as={Link} to="/shop" href="#shop">Shop</Nav.Link>
                         </Nav>
+                        <Nav className='me-auto'>
+                            {
+                                user &&
+                                <>
+                                    <Nav.Link>{user?.displayName} </Nav.Link>
+                                    <Navbar.Brand>
+                                        <img style={{'borderRadius': '50%'}} src={user?.photoURL} alt="" />
+                                    </Navbar.Brand>
+                                </>
+                            }
+                        </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="/favorite" href="#favorite">Favorite item</Nav.Link>
-                            <Nav.Link eventKey={2} as={Link} to="/login" href="#signin">
-                                Login
-                            </Nav.Link>
+                            {
+                                user
+                                    ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                    :
+                                    <Nav.Link eventKey={2} as={Link} to="/login" href="#signin">
+                                        Login
+                                    </Nav.Link>
+
+                            }
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
