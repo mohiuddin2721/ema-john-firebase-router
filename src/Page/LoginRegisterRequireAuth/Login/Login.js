08a236ed6296265c14f8ca63a12328e7addc2473
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-16 12:40:11 +0600

    google signin implement

--- src/Page/LoginRegisterRequireAuth/Login/Login.js
+++ src/Page/LoginRegisterRequireAuth/Login/Login.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import auth from '../../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+
+
+    const handleSubmit = () => {
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+    }
+
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
                     Login
--- src/Page/LoginRegisterRequireAuth/SocialLogin/SocialLogin.js
+++ src/Page/LoginRegisterRequireAuth/SocialLogin/SocialLogin.js
@@ -2,8 +2,29 @@ import React from 'react';
 import facebook from '../../../images/facebook.png';
 import google from '../../../images/google.png';
 import gitPic from '../../../images/github.png';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
 
 const SocialLogin = () => {
+
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, gitUser, gitLoading, gitError] = useSignInWithGithub(auth);
+    const navigate = useNavigate();
+
+    let errorMassage;
+
+    if (googleLoading || gitLoading) {
+        return <Loading></Loading>
+    }
+    if (googleError || gitError) {
+        errorMassage = <p className='text-danger'>Error: {googleError?.message} {gitError?.message}</p>
+    }
+    if (googleUser || gitUser) {
+        navigate('/home');
+    }
+
     return (
         <div>
             <div>
@@ -12,17 +33,22 @@ const SocialLogin = () => {
                 <p className='mt-2 px-2'>or</p>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
+            {errorMassage}
             <div className=''>
                 <button
-                    className='btn btn-info w-50 d-block mx-auto my-2'>
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-info w-50 d-block mx-auto mx-2 my-2'>
                     <img style={{ width: '30px' }} src={google} alt="" />
                     <span className='px-2'>Google Sign In</span>
                 </button>
-                <button className='btn btn-info w-50 d-block mx-auto my-2'>
+                <button 
+
+                    className='btn btn-info w-50 d-block mx-auto my-2'>
                     <img style={{ width: '30px' }} src={facebook} alt="" />
                     <span className='px-2'>Facebook Sign In</span>
                 </button>
                 <button
+                    onClick={() => signInWithGithub()}
                     className='btn btn-info w-50 d-block mx-auto'>
                     <img style={{ width: '30px' }} src={gitPic} alt="" />
                     <span className='px-2'>Github Sign In</span>
